ff04a20d06c85b22e0da832a25e84691b9803c5f
jobastico <jobatico0987@gmail.com>
2021-11-09 09:29:34 -0300

    criando constant.dart

---
+++ flutter_application_1/lib/core/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const int BOARD_SIZE = 9;
+
+const Color PLAYER1_COLOR = Colors.red;
+const Color PLAYER2_COLOR = Colors.blue;
+const String PLAYER1_SYMBOL = "x";
+const String PLAYER2_SYMBOL = 'O';
+
+const String GAME_TTLE = "Jogo do Bicho";
+const String GAME_TIED = "Game Tied!";
+const String WIN_TITLE = "Player [SYMBOL] won!";
+const String DIALOG_MESSAGE = "Press the reset button to start";
+const String SINGLE_PLAYER_MODE_LABEL = "Single Player";
+const String MULTIPLAYER_MODE_LABEL = "Tow Player";
+const String RESET_BUTTON_LABEL = "Reset";
